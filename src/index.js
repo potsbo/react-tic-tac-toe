28669a2f275b9820f77ecb7cdab834650c710aa5
Shimpei Otsubo <potsbo@gmail.com>
2017-04-23 03:58:29 +0900

    Lift up states

--- src/index.js
+++ src/index.js
@@ -19,8 +19,19 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
   render() {
     const status = 'Next player: X';
