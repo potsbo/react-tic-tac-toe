2570fb5a414ac50a90a479b692e55c641c68088d
Shimpei Otsubo <potsbo@gmail.com>
2017-04-23 04:26:31 +0900

    Add state

--- src/index.js
+++ src/index.js
@@ -76,13 +76,24 @@ class Game extends React.Component {
     };
   }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
           <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
